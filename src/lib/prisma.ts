e3225e21f44a96c13f2915b7180a6f49cf15a4b7
Akenarin Komkoon <codingthaialnd@gmail.com>
2026-06-04 22:49:12 +0700

    add prisma orm v7

---
+++ src/lib/prisma.ts
@@ -0,0 +1,25 @@
+import "dotenv/config";
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+import { PrismaClient } from "../../generated/prisma/client";
+
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient; 
+}; 
+
+const adapter = new PrismaMariaDb({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  port: Number(process.env.DATABASE_PORT),
+  connectionLimit: 5,
+}); 
+
+const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter, 
+  }); 
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma; 
+
+export { prisma };
